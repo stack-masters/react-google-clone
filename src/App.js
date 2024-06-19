61890c6cf62b6e27df402fd18ff6dbb3a2f570fc
Boseok Son <dyacode@proton.me>
2024-06-19 19:37:07 +0900

    a

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css'
 import { useState } from 'react'
 import Logo from './img/logo.png'
 import Search from './img/search.svg'
+import Mic from './img/mic.svg'
+import Camera from './img/camera.svg'
 
 function App() {
-  
   const [value, setValue] = useState('');
 
   const onSubmitHandler = (e) => {
@@ -15,11 +16,17 @@ function App() {
   return (
     <div className='main'>
       <img className='logo' src={Logo} />
+
       <form className='searchbar' onSubmit={onSubmitHandler}>
-        <img className='search' src={Search} />
+        <img className='search' src={Search} onClick={onSubmitHandler} />
+
         <input type="text" placeholder="Search ...."
                 value={value} onChange={(e) => setValue(e.target.value)} />
+
+        <img className='mic' src={Mic} />
+        <img className='camera' src={Camera} />
       </form>
+
     </div>
   )
 }
